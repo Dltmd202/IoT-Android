c54de4db7e0e662f705d12b37992c962faba69b7
Dltmd202 <dltmd202@gmail.com>
2021-12-07 03:12:15 +0900

    init proj

---
+++ app/src/main/java/org/iot/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package org.iot.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button openButton = findViewById(R.id.open);
+
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    public void makeRequest(){
+        String url = "192.168.0.35:8000/";
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+
+    }
+}
---
+++ app/src/main/java/org/iot/myapplication/Window.java
@@ -0,0 +1,9 @@
+package org.iot.myapplication;
+
+public class Window {
+    String id;
+    String distance;
+    String temperature;
+    String humidity;
+    String is_person;
+}
